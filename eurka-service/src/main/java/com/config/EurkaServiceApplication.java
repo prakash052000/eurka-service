42381f5d9006ebca9f6e72c6082d5fe3c560427c
prakash052000 <prakashmaniraj07@gmail.com>
2025-03-27 23:08:23 +0530

    Your commit message

---
+++ eurka-service/src/main/java/com/config/EurkaServiceApplication.java
@@ -0,0 +1,17 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class EurkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurkaServiceApplication.class, args);
+	}
+
+}
